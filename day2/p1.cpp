543f0a42ae5ae4a98b418dcd5cee18218e0d9ca7
udgirkarambarish <udgirkarambarish@gmail.com>
2023-02-03 23:59:30 +0530

    d1p1

--- day2/p1.cpp
+++ day2/p1.cpp
@@ -1,5 +1,21 @@
 #include <iostream>
 using namespace std;
-int main() {
 
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int n,nrem=0,d[1000];
+	    
+	    cin>>n;
+	    for(int i=0;i<n;i++){
+	        cin>>d[i];
+	        if(d[i]>=1000){
+	            nrem++;
+	        }
+	    }
+	    cout<<nrem<<endl;
+	}
+	return 0;
 }
